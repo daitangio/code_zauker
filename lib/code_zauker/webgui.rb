4a81c3782d13515442fe108cab4a6b0f2475eabc
Giovanni Giorgi <jj@gioorgi.com>
2012-02-28 12:56:33 +0100

    Checkin of code zauker web assents. Fix redis paths

--- lib/code_zauker/webgui.rb
+++ lib/code_zauker/webgui.rb
@@ -1,12 +1,49 @@
 #!/usr/bin/env ruby
 require 'sinatra'
+require "code_zauker/version"
+require "code_zauker"
+require "erb"
+
 # See http://www.sinatrarb.com/intro
 get '/' do
   # Show the search box...
-  'Hello world!'
+  erb :search
 end
 
-get '/search/:q' do
+get '/search' do
   # Process the search and show the results...
-  "Results for #{params[:q]}..."
+  fs=CodeZauker::FileScanner.new()
+  files=fs.isearch(params[:q])
+  # Example of locals usage: erb "<%= foo %>", :locals => {:foo => "bar"}
+  "Results for #{params[:q]}...\n#{files}"
+  
 end
+
+configure do
+  staticDir=File.dirname(__FILE__)+ '/../../htdocs'
+  templateDir=settings.root + '/../../templates'
+  puts "Static files: #{staticDir}"
+  puts "Templates: #{templateDir}"
+  set :public_folder, staticDir
+  set :views, templateDir
+
+end
+
+# INLINE Template Follows
+__END__
+
+@@ index
+<html>
+<head>
+<title> CodeZauker v<%= CodeZauker::VERSION %> </title>
+</head>
+<body>
+  <div align="left" style="background:url(/CodeZauker.gif) no-repeat; width:489; height:110;" title="CodeZauker">
+  Code Zauker
+  </div>  
+  <form method="get" action="/search">
+   <input type="text" name="q" />
+  </form>
+</body>
+
+</html>
