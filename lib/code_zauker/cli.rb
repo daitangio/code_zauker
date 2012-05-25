31d22b6516746976e59a4d9e27ef94a8c642aa6d
Giovanni Giorgi <jj@gioorgi.com>
2012-05-25 11:24:43 +0200

    Bug fix for wild insensitive search

--- lib/code_zauker/cli.rb
+++ lib/code_zauker/cli.rb
@@ -2,6 +2,7 @@ module CodeZauker
 
   class CliUtil
 
+    # Create a regexp to do a case insensitive wild search used by grep
     def doWildSearch(term,fileScanner)
       fileGroup=fileScanner.wsearch(term)
       # Make a simple regexp from the wild stuff...
@@ -10,7 +11,7 @@ module CodeZauker
         finalRegexp= finalRegexp+Regexp.escape(term)+".*"
       end
       return {
-        :regexp=>finalRegexp,
+        :regexp=>/#{finalRegexp}/i,
         :files => fileGroup
       }
       
